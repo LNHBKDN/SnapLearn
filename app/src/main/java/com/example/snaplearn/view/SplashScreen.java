c29e8b8b86e6aa446caed2e4e2300311d14d0114
JustQtie <qtiepiece@gmail.com>
2023-12-15 19:40:33 +0700

    add dummy sound

--- app/src/main/java/com/example/snaplearn/view/SplashScreen.java
+++ app/src/main/java/com/example/snaplearn/view/SplashScreen.java
@@ -1,6 +1,7 @@
 package com.example.snaplearn.view;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.animation.Animation;
@@ -12,16 +13,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.snaplearn.R;
 
 public class SplashScreen extends AppCompatActivity {
+    private MediaPlayer mediaPlayer;
     ImageView splashImageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
 
+        mediaPlayer = MediaPlayer.create(this,R.raw.dummy_sound);
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                mediaPlayer.release();
+            }
+        });
 
+        // Start playing the sound
+        mediaPlayer.start();
         splashImageView = findViewById(R.id.splash_image_view);
         Animation splash_animation = AnimationUtils.loadAnimation(this, R.anim.splash_animation);
         splashImageView.startAnimation(splash_animation);
+
 
         splash_animation.setAnimationListener(new Animation.AnimationListener() {
             @Override
@@ -49,5 +61,17 @@ public class SplashScreen extends AppCompatActivity {
 //                finish();
 //            }
 //        },3000);
+
     }
-}
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // Release MediaPlayer resources when the activity is destroyed
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+
+
+}}

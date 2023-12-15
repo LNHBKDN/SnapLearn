50d1ee0dee12a04b758e7fba785006b18bf77fe9
JustQtie <qtiepiece@gmail.com>
2023-12-15 17:57:09 +0700

    modified

--- app/src/main/java/com/example/snaplearn/view/Register.java
+++ app/src/main/java/com/example/snaplearn/view/Register.java
@@ -3,7 +3,9 @@ package com.example.snaplearn.view;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -32,7 +34,11 @@ public class Register extends AppCompatActivity {
 //            finish();
 //        }
 //    }
-
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        moveTaskToBack(false);
+    }
     @Override
     protected void onPostCreate(@Nullable Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
@@ -42,6 +48,17 @@ public class Register extends AppCompatActivity {
         editTextPassword = findViewById(R.id.password);
         reEditTextPassword = findViewById(R.id.repassword);
         buttonReg = findViewById(R.id.btn_register);
+
+        TextView textView;
+        textView = findViewById(R.id.tv_login);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
         buttonReg.setOnClickListener(v -> {
             String email, password, rePassword;
